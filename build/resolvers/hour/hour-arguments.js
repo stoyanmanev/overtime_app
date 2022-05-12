12258b86b490213b4c6fa859684e783ec695d540
Stoyan Manev <61543561+stoyanmanev@users.noreply.github.com>
2022-05-12 13:21:55 +0300

    add build

---
+++ build/resolvers/hour/hour-arguments.js
@@ -0,0 +1,60 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HourInput = exports.EditHourInput = void 0;
+const class_validator_1 = require("class-validator");
+const mongodb_1 = require("mongodb");
+const type_graphql_1 = require("type-graphql");
+let EditHourInput = class EditHourInput {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    (0, class_validator_1.MinLength)(6),
+    __metadata("design:type", String)
+], EditHourInput.prototype, "date", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Number)
+], EditHourInput.prototype, "value", void 0);
+__decorate([
+    (0, type_graphql_1.Field)({ nullable: true }),
+    __metadata("design:type", String)
+], EditHourInput.prototype, "description", void 0);
+EditHourInput = __decorate([
+    (0, type_graphql_1.InputType)()
+], EditHourInput);
+exports.EditHourInput = EditHourInput;
+let HourInput = class HourInput {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", mongodb_1.ObjectId)
+], HourInput.prototype, "_id", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], HourInput.prototype, "date", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", Number)
+], HourInput.prototype, "value", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], HourInput.prototype, "createdBy", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], HourInput.prototype, "description", void 0);
+HourInput = __decorate([
+    (0, type_graphql_1.InputType)()
+], HourInput);
+exports.HourInput = HourInput;
---
+++ build/resolvers/hour/hour-resolver.js
@@ -0,0 +1,87 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HourResolver = void 0;
+const type_graphql_1 = require("type-graphql");
+const apollo_server_core_1 = require("apollo-server-core");
+const hour_entity_1 = require("../../entities/hour-entity");
+const hour_arguments_1 = require("./hour-arguments");
+const user_roles_1 = require("../user/user.roles");
+let HourResolver = class HourResolver {
+    async hours() {
+        return await hour_entity_1.HourModel.find({});
+    }
+    async hour(_id) {
+        return await hour_entity_1.HourModel.findById(_id);
+    }
+    async createHour(data, ctx) {
+        if (!ctx.user) {
+            throw new apollo_server_core_1.AuthenticationError('user_not_authenticated');
+        }
+        const hourData = {
+            ...data,
+            createdBy: ctx.user,
+        };
+        const newHour = new hour_entity_1.HourModel(hourData);
+        await newHour.save();
+        return newHour;
+    }
+    async deleteHour(_id) {
+        return await hour_entity_1.HourModel.findByIdAndRemove(_id);
+    }
+    async editHour(_id, data) {
+        return await hour_entity_1.HourModel.findByIdAndUpdate(_id, data, { new: true });
+    }
+};
+__decorate([
+    (0, type_graphql_1.Query)((returns) => [hour_entity_1.Hour]),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", Promise)
+], HourResolver.prototype, "hours", null);
+__decorate([
+    (0, type_graphql_1.Query)((returns) => hour_entity_1.Hour),
+    __param(0, (0, type_graphql_1.Arg)("_id")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], HourResolver.prototype, "hour", null);
+__decorate([
+    (0, type_graphql_1.Mutation)((returns) => hour_entity_1.Hour),
+    __param(0, (0, type_graphql_1.Arg)("data")),
+    __param(1, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [hour_arguments_1.EditHourInput, Object]),
+    __metadata("design:returntype", Promise)
+], HourResolver.prototype, "createHour", null);
+__decorate([
+    (0, type_graphql_1.Authorized)([user_roles_1.UserRoles.ADMIN, user_roles_1.UserRoles.SUPER_ADMIN]),
+    (0, type_graphql_1.Mutation)((returns) => hour_entity_1.Hour),
+    __param(0, (0, type_graphql_1.Arg)("_id")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], HourResolver.prototype, "deleteHour", null);
+__decorate([
+    (0, type_graphql_1.Mutation)((returns) => hour_entity_1.Hour),
+    __param(0, (0, type_graphql_1.Arg)("_id")),
+    __param(1, (0, type_graphql_1.Arg)("data")),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String, hour_arguments_1.EditHourInput]),
+    __metadata("design:returntype", Promise)
+], HourResolver.prototype, "editHour", null);
+HourResolver = __decorate([
+    (0, type_graphql_1.Resolver)()
+], HourResolver);
+exports.HourResolver = HourResolver;
